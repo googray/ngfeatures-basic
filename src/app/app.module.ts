4bc0b4826dc029f47427b54626e95871f6c7be0f
googray <shat.rom07@gmail.com>
2022-12-13 23:49:46 +0100

    add new data binding with two way

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,18 @@ import { NavComponent } from './nav/nav.component';
 import { HeaderComponent } from './header/header.component';
 import { NotificationComponent } from './notification/notification.component';
 import { SearchComponent } from './search/search.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [AppComponent, ContainerComponent, NavComponent, HeaderComponent, NotificationComponent, SearchComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    ContainerComponent,
+    NavComponent,
+    HeaderComponent,
+    NotificationComponent,
+    SearchComponent,
+  ],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
