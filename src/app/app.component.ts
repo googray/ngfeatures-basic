aa3a9a0407da4f1a224cd7070bf53132cbccb0ec
googray <shat.rom07@gmail.com>
2022-12-22 00:03:31 +0100

    one of case use template reference

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { DemoComponent } from './demo/demo.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'prodac-component';
+
+  @ViewChild('dobInput') public dateOfBirth!: ElementRef;
+  @ViewChild('ageInput') public age!: ElementRef;
+  @ViewChild(DemoComponent, { static: true }) public demoComp!: DemoComponent;
+
+  calculateAge() {
+    let birthYear = new Date(
+      this.dateOfBirth?.nativeElement.value
+    ).getFullYear();
+    let currentYear = new Date().getFullYear();
+    let age = currentYear - birthYear;
+    this.age.nativeElement.value = age;
+    //   console.log(this.dateOfBirth);
+    //   console.log(this.age);
+  }
 }
