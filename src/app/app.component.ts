6232e15d9da1c3983a1912f6df909a4ad0199360
googray <shat.rom07@gmail.com>
2022-12-28 23:29:12 +0100

    used&explain all ng hooks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'prodac-component';
+
+  public inputText: string = '';
+  public destroy: boolean = true;
+
+  OnSubmit(inputEl: HTMLInputElement) {
+    this.inputText = inputEl.value;
+  }
+
+  DestroyComponent() {
+    this.destroy = false;
+  }
 }
---
+++ src/app/demo/demo.component.ts
@@ -0,0 +1,71 @@
+import {
+  Component,
+  Input,
+  OnInit,
+  OnChanges,
+  SimpleChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy,
+} from '@angular/core';
+
+@Component({
+  selector: 'app-demo',
+  templateUrl: './demo.component.html',
+  styleUrls: ['./demo.component.css'],
+})
+export class DemoComponent
+  implements
+    OnInit,
+    OnChanges,
+    DoCheck,
+    AfterContentInit,
+    AfterContentChecked,
+    AfterViewInit,
+    AfterViewChecked,
+    OnDestroy
+{
+  @Input() public valueO: string = 'procademy';
+
+  constructor() {
+    console.log('Constructor called!');
+    // console.log(this.valueO);
+  }
+
+  ngOnChanges(change: SimpleChanges) {
+    console.log('ngOnChanges called!');
+    console.log(change);
+  }
+
+  ngOnInit(): void {
+    console.log('ngOnInit called!');
+    // console.log(this.valueO);
+  }
+
+  ngDoCheck() {
+    console.log('ngDoCheck called!');
+  }
+
+  ngAfterContentInit() {
+    console.log('ngAfterContentInit called!');
+  }
+
+  ngAfterContentChecked() {
+    console.log('ngAfterContentChecked called!');
+  }
+
+  ngAfterViewInit() {
+    console.log('ngAfterViewInit called!');
+  }
+
+  ngAfterViewChecked() {
+    console.log('ngAfterViewChecked called!');
+  }
+
+  ngOnDestroy() {
+    console.log('ngOnDestroy called!');
+  }
+}
