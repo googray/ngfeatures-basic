ab4bbd390b7fc115a0971130ccf54474b7f105d3
googray <shat.rom07@gmail.com>
2023-01-11 22:56:49 +0100

    passing fragments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'AngularRouting';
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.route.fragment.subscribe((value) => {
+      console.log(value);
+      this.jumpTo(value);
+    });
+  }
+
+  jumpTo(section: string | null) {
+    document.getElementById(section!)?.scrollIntoView({ behavior: 'smooth' });
+  }
 }
