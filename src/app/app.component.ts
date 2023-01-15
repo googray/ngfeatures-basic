c4c6e93d122da5a08c9f8133b692aa6893fc921b
googray <shat.rom07@gmail.com>
2023-01-15 21:28:02 +0100

    init pipe study proj

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { IStudent } from './student';
+import { StudentService } from './students.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [StudentService],
 })
 export class AppComponent {
   title = 'prodac-component';
+
+  students!: IStudent[];
+  totalMarks!: number;
+
+  ngOnInit() {
+    this.students = this.studentService.students;
+    this.totalMarks = this.studentService.totalMarks;
+  }
+
+  constructor(private studentService: StudentService) {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PercentagePipe } from './percentage.pipe';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, PercentagePipe],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
