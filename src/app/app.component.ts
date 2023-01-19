5c696bb8656b2978be38ed2e1d272f6bfccb6f5e
googray <shat.rom07@gmail.com>
2023-01-19 22:29:37 +0100

    radio button work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, OnChanges, SimpleChanges, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -6,15 +6,24 @@ import { NgForm } from '@angular/forms';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnChanges {
   title = 'prodac-component';
   defaultCountry = 'india';
   firstname!: string;
   lastname!: string;
+  defaultGender = 'Male';
+
+  gender = [
+    { id: '1', value: 'Male' },
+    { id: '2', value: 'Female' },
+    { id: '3', value: 'Other' },
+  ];
 
   @ViewChild('myForm') form!: NgForm;
 
   onSubmit() {
     console.log(this.form);
   }
+
+  ngOnChanges(): void {}
 }
