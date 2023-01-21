778706706e12d60f424818f475befbaa38b6f87d
googray <shat.rom07@gmail.com>
2023-01-21 15:11:56 +0100

    init reactive form method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
+import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'prodac-component';
+
+  reactiveForm: FormGroup;
+
+  ngOnInit() {
+    this.reactiveForm = new FormGroup({
+      firstname: new FormControl(null),
+      lastname: new FormControl(null),
+      email: new FormControl(null),
+      gender: new FormControl('male'),
+      country: new FormControl('india'),
+      hobbies: new FormControl(null),
+    });
+  }
+
+  onSubmit() {
+    console.log(this.reactiveForm);
+  }
 }
