f01c50a710431d8127fc599e21cfe6e671c61a4e
googray <shat.rom07@gmail.com>
2023-01-27 00:16:46 +0100

    init new proj about http, used POST request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
+
+export interface IProductCreate {
+  pName: string;
+  desc: string;
+  price: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'prodac-component';
+
+  constructor(private http: HttpClient) {}
+
+  onProductCreate(products: IProductCreate) {
+    console.log(products);
+    const hHeaders = new HttpHeaders({ myHeader: 'graygoo' });
+    this.http
+      .post(
+        'https://ngfeatures-general-concepts-default-rtdb.firebaseio.com/product.json',
+        products,
+        { headers: hHeaders }
+      )
+      .subscribe((res) => console.log(res));
+  }
 }
