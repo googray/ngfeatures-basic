e729f60fb41610a8f45cab208ca101c47755ad6e
googray <shat.rom07@gmail.com>
2023-01-27 19:46:47 +0100

    used GET request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,58 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Component } from '@angular/core';
-
-export interface IProductCreate {
-  pName: string;
-  desc: string;
-  price: string;
-}
+import { Component, OnInit } from '@angular/core';
+import { map } from 'rxjs';
+import { IProductCreate } from './model/products';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'prodac-component';
+  allProducts: IProductCreate[] = [];
 
   constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.fetchProducts();
+  }
+
+  onProductsFetch() {
+    this.fetchProducts();
+  }
 
   onProductCreate(products: IProductCreate) {
     console.log(products);
     const hHeaders = new HttpHeaders({ myHeader: 'graygoo' });
     this.http
-      .post(
+      .post<{ name: string }>(
         'https://ngfeatures-general-concepts-default-rtdb.firebaseio.com/product.json',
         products,
         { headers: hHeaders }
       )
       .subscribe((res) => console.log(res));
+  }
+
+  private fetchProducts() {
+    this.http
+      .get<{ [key: string]: IProductCreate }>(
+        'https://ngfeatures-general-concepts-default-rtdb.firebaseio.com/product.json'
+      )
+      .pipe(
+        map((res) => {
+          const products = [];
+          for (const key in res) {
+            if (res.hasOwnProperty(key)) {
+              products.push({ ...res[key], id: key });
+            }
+          }
+          return products;
+        })
+      )
+      .subscribe((products) => {
+        console.log(products);
+        this.allProducts = products;
+      });
   }
 }
